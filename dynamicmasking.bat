1aef06eb94164a7e5d49d378101090b2f84f78e6
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-04-21 16:24:22 +1000

    Update dynamicmasking.bat

--- dynamicmasking.bat
+++ dynamicmasking.bat
@@ -1,5 +1,4 @@
 echo off
-REM  The path to SQLCMD varies according to version, it might not be 130
 
 if "%ACT_MULTI_OPNAME%" == "scrub-mount" if "%ACT_MULTI_END%" == "true" if "%ACT_PHASE%" == "post" ( GOTO maskcommand )
 if "%ACT_MULTI_OPNAME%" == "mount" if "%ACT_MULTI_END%" == "true" if "%ACT_PHASE%" == "post" ( GOTO maskcommand )
@@ -9,7 +8,7 @@ if "%1" == "" echo If you want to manually run this command add test after the b
 exit /B 0
 
 :maskcommand
-"C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE" -b -i "C:\Program Files\Actifio\scripts\%2"
+SQLCMD.EXE -b -i "C:\Program Files\Actifio\scripts\%2"
 IF %ERRORLEVEL% EQU 0 (GOTO cleanexit)
 exit /B 1
 
