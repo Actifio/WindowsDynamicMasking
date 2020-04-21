a06cf77657812382ba137fc258c546a136f58674
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-04-21 15:57:58 +1000

    Update dynamicmasking.bat

--- dynamicmasking.bat
+++ dynamicmasking.bat
@@ -9,7 +9,7 @@ if "%1" == "" echo If you want to manually run this command add test after the b
 exit /B 0
 
 :maskcommand
-"C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE" -b -i "C:\Program Files\Actifio\scripts\dynamicmasking.sql"
+"C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE" -b -i "C:\Program Files\Actifio\scripts\%2"
 IF %ERRORLEVEL% EQU 0 (GOTO cleanexit)
 exit /B 1
 
